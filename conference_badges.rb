11959fa4561976b66882e7df0a8b5321cebb4da1
Josh Ellis <joshe523@gmail.com>
2020-03-02 22:19:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name_arr)
+  badge_arr = []
+  name_arr.each do |name|
+    badge_arr << badge_maker(name)
+  end
+  return badge_arr
+end
